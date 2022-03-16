ac8aab37e3da79341de32e2c8b604f12e37dba34
FabianMatata <mfabian601@gmail.com>
2022-03-16 12:41:21 +0300

    MESSAGE

--- awwards/views.py
+++ awwards/views.py
@@ -14,13 +14,13 @@ from .serializer import ProfileSerializer,ProjectSerializer
 
 # Create your views here.
 def home(request):
-    projects=Project.objects.all()
-    return render(request,"awwards/home.html",{'projects':projects})
+    project=Project.objects.all()
+    return render(request,"awwards/home.html",{'project':project})
 
 @login_required(login_url='/accounts/login/') 
 def rate_project(request,project_id):
     project=Project.objects.get(id=project_id)
-    return render(request,"awwards/project.html",{'projects':projects})
+    return render(request,"awwards/project.html",{'project':project})
 
 @login_required(login_url='/accounts/login/') 
 def view_profile(request):
