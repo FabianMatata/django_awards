f3c91496c3b0100cec4dbb6c8d0e086435f4b151
FabianMatata <mfabian601@gmail.com>
2022-03-16 10:06:09 +0300

    deploy heroku

--- projects/settings.py
+++ projects/settings.py
@@ -65,7 +65,9 @@ SECRET_KEY = '7=0zmldf)t4h0gd1d9y)#qfr4sa5otu*6jnoeuql@-25as!j!v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [https://django-awards.herokuapp.com/]
+ALLOWED_HOSTS = [
+    'https://django-awards.herokuapp.com/'
+    ]
 
 
 # Application definition
